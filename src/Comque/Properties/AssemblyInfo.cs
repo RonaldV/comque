3f9a78bbe9ef2df260ce6021302cc695a5d9b02c
Ronald Verhaegen <ronald@softelligent.be>
2015-03-23 17:19:32 +0100

    Changed assembly description

--- src/Comque/Properties/AssemblyInfo.cs
+++ src/Comque/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Comque")]
-[assembly: AssemblyDescription("Command Query execution library using the mediator pattern.")]
+[assembly: AssemblyDescription("Command query execution library using the mediator pattern.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ronald Verhaegen")]
 [assembly: AssemblyProduct("Comque")]
